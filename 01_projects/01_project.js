41c1470626a0564b17f0ad024e7e48f847168b5e
surajrathorerocks <131504529+surajrathorerocks@users.noreply.github.com>
2025-06-26 08:48:26 +0000

    first project done

--- 01_projects/01_project.js
+++ 01_projects/01_project.js
@@ -1,2 +1,22 @@
-console.log("suraj");
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector("body")
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+   if(e.target.id=='Grey'){
+   body.style.backgroundColor = e.target.id;
+   }
+    if(e.target.id=='White'){
+   body.style.backgroundColor = e.target.id;
+   }
+    if(e.target.id=='blue'){
+   body.style.backgroundColor = e.target.id;
+   }
+    if(e.target.id=='Yellow'){
+   body.style.backgroundColor = e.target.id;
+   }
+     if(e.target.id=='Skyblue'){
+   body.style.backgroundColor = e.target.id;
+   }
+    })
+})
 
